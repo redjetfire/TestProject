60b43a041d57c68618a3bc406c5001c5eb76b7db
JetFire <RedJetFire@gmail.com>
2012-11-26 23:44:36 +0200

    First

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,10 @@
+class ApplicationController < ActionController::Base
+  protect_from_forgery
+  before_filter :load_all_var
+  
+  def load_all_var
+    @categories = Category.order(:name)
+    @lastcomments = Comment.order(:id).reverse_order.limit(5)
+    @news = Category.order(:id).reverse_order.limit(5)
+    end
+end
